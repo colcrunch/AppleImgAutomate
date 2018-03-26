9d5ee9bab5d7e317a8b9f8fccfe99d1fd65c9a20
colcrunch <rhartnett35@gmail.com>
2018-03-25 20:58:42 -0400

    New Site, Fresh script.

--- proc.py
+++ proc.py
@@ -2,57 +2,35 @@ from PIL import Image
 import re
 import glob, os
 import requests
-from contextlib import closing
-from bs4 import BeautifulSoup
-from selenium.webdriver import Firefox
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import json
 
-src = "url_redacted"
-regex = r'[S][T][0-9]{8}'
-regex2  = ''
-binary = FirefoxBinary("C:\Program Files (x86)\Mozilla Firefox\firefox.exe")
+src = "http://10.1.10.49:2235/showtimes.json"
 
-with closing(Firefox(firefox_binary=binary)) as browser:
-    browser.get(src)
-    WebDriverWait(browser, timeout=10)
-    page_source = browser.page_source
+shows = requests.get(src)
+json = shows.json()
 
-with open("output2.html", "w", encoding="utf-8") as file:
-    file.write(page_source)
+films = json['films']
 
-soup = BeautifulSoup(open("output2.html", encoding="utf-8"), 'html.parser')
-
-dP = soup.find_all("div", "panel")
 tS = []
 iS = []
 
-for d in dP:
-    t = d.find_all("p")
-    v = d.find_all("img", {'height': '200px'})
-    for w in v:
-        i = re.search(regex, w['src'])
-        if i is not None:
-            iS.append(i[0])
-        else:
-            iS.append('N')
-    for u in t:
-        tS.append(u.text)
-print(tS)
-print(iS)
-movs = dict(zip(tS, iS))
-print(movs)
-    
+for film in films:
+    tS.append(film['name'])
+    iS.append(film['RtsCode'])
+
+movs = dict(zip(tS,iS))
+
 for infile in glob.glob("*.jpg"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
     t = im.resize((185, 278), Image.BILINEAR)
     f = im.resize((450, 675), Image.BILINEAR)
+    file = file.upper()
     if file in movs:
         print(file + movs[file])
         f.save("sn"+movs[file]+".jpg", "JPEG")
-        print(file+" >> "+"s"+movs[file]+".jpg")
-        t.save("s"+movs[file]+".jpg", "JPEG")
+        print(file+" >> "+"sS"+movs[file]+".jpg")
+        t.save("s"+movs[file]+".move", "JPEG")
         print(file+" >> "+"sn"+movs[file]+".jpg")
     else:
         f.save(file+"-_-screen.jpg", "JPEG")
@@ -61,8 +39,9 @@ for infile in glob.glob("*.jpg"):
         print(file+" > > "+file+"-_-movie.jpg")
 
 os.system("move sn*.jpg screens")
-os.system("move sS*.jpg movies")
+os.system("move s*.move movies")
 os.system("move *-_-screen.jpg screens")
 os.system("move *-_-movie.jpg movies")
 os.system("move *.jpg originals")
-os.system("del output2.html")
+os.system("cd movies && ren *.move *.jpg")
+
